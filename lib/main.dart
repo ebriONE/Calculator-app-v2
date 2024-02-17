7a4be4fac4d0d32b86c4a96eaa6d2212382e6b97
Dmytro <az09.uatop@gmail.com>
2024-02-17 11:58:15 +0200

    fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,186 +1,201 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
-
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   State<HomePage> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<HomePage> {
+
+  int valueOfTheNumber = 0;
+
   @override
-  Widget build(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text("Calculator"),
-          centerTitle: false,
-        ),
-        backgroundColor: Colors.deepPurpleAccent,
-        body:
-           Row(
-             children: [
-               Column(
-                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                 crossAxisAlignment: CrossAxisAlignment.start, children: [
-                   Padding(
-                  padding: const EdgeInsets.all(8.0),
-                     child: SizedBox(
-                       width: 70,
-                       height: 40,
-                       child: TextButton(onPressed: (){},
-                        style: TextButton.styleFrom(
-                          side: const BorderSide( width: 1.0)),
-                        child:  const Text("7"),
-                    ),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: 70,
-                    height: 40,
-                    child: TextButton(onPressed: (){},
-                        style: TextButton.styleFrom(
-                            side: const BorderSide(width: 1.0)),
-                        child: const Text("4")
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calculator"),
+        centerTitle: false,
+      ),
+      backgroundColor: Colors.purple[100],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 450,
+            height: 410,
+            child: Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: SizedBox(
+                width: 30,
+                child: Container(
+                  color: Colors.pink[50],
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20.0,330.0,20.0,4.0),
+                    child: Text(
+                      "$valueOfTheNumber",
+                      textDirection: TextDirection.rtl,
+                      style: TextStyle(
+                        fontSize: 40.0
+                      ),
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0,8.0,8.0,8.0),
-                  child: SizedBox(
-                    width: 70,
-                    height: 40,
-                    child: TextButton(onPressed: (){},
-                        style: TextButton.styleFrom(
-                          side: const BorderSide(width: 1.0)),
-                        child: const Text("1")),
-                    //end of first column
-                  ),
-                )]
-        ),
-              Column(
-               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-               crossAxisAlignment: CrossAxisAlignment.center,
-               children: [
-                 Padding(
-                   padding: const EdgeInsets.fromLTRB(8.0,8.0,8.0,8.0),
-                   child: SizedBox(
-                     width: 70,
-                     height: 40,
-                     child: TextButton(onPressed: (){},
-                         style: TextButton.styleFrom(
-                           side: const BorderSide(width: 1.0)
-                         ),
-                         child: const Text("8")
-                     ),
-                   ),
-                 ),
-                 Padding(
-                   padding: const EdgeInsets.fromLTRB(8.0,8.0,8.0,8.0),
-                   child: SizedBox(
-                     width: 70,
-                     height: 40,
-                     child: TextButton(onPressed: (){},
-                         style: TextButton.styleFrom(
-                             side: const BorderSide(width: 1.0)
-                         ),
-                         child: const Text("8")
-                     ),
-                   ),
-                 ),
-                 Padding(
-                   padding: const EdgeInsets.fromLTRB(8.0,8.0,8.0,8.0),
-                   child: SizedBox(
-                     width: 70,
-                     height: 40,
-                     child: TextButton(onPressed: (){},
-                         style: TextButton.styleFrom(
-                             side: const BorderSide(width: 1.0)
-                         ),
-                         child: const Text("8")
-                     ),
-                   ),
-                 ),
-                 Padding(
-                   padding: const EdgeInsets.fromLTRB(8.0,8.0,8.0,8.0),
-                   child: SizedBox(
-                     width: 70,
-                     height: 40,
-                     child: TextButton(onPressed: (){},
-                       style: TextButton.styleFrom(
-                         side: const BorderSide(width: 1.0)
-                       ),
-                         child: const Text("8"),
-                     ),
-                   ),
-                 )
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildButton("C", () {
+                setState(() {
+                  valueOfTheNumber = 0;
+                });
+              }),
+              buildButton("(  )", () {
+                //TODO add action
+              }),
+              buildButton("%", () {
+                //TODO add action
+              }),
+              buildButton("/", () {
+                //TODO add action
+              }),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildButton("7",() {
+                setState(() {
+                  valueOfTheNumber = 7;
+                });
+              }),
+              buildButton("8",() {
+                setState(() {
+                  valueOfTheNumber = 8;
+                });
+              }),
+              buildButton("9",() {
+                setState(() {
+                  valueOfTheNumber = 9;
+                });
+              }),
+              buildButton("*", () {
+                //TODO add action
+              }),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildButton("4",() {
+                setState(() {
+                  valueOfTheNumber = 4;
+                });
+              }),
+              buildButton("5",() {
+                setState(() {
+                  valueOfTheNumber = 5;
+                });
+              }),
+              buildButton("6",() {
+                setState(() {
+                  valueOfTheNumber = 6;
+                });
+              }),
+              buildButton("-", () {
+                //TODO add action
+              }),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildButton("1",() {
+                setState(() {
+                  valueOfTheNumber = 1;
+                });
+              }),
+              buildButton("2",() {
+                setState(() {
+                  valueOfTheNumber = 2;
+                });
+              }),
+              buildButton("3",() {
+                setState(() {
+                  valueOfTheNumber = 3;
+                });
+              }),
+              buildButton("+", () {
+                setState(() {
+                  //TODO add action
+                });
+              }),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildButton("+/-", () {
+                //TODO add action
+              }),
+              buildButton("0",() {
+                setState(() {
+                  valueOfTheNumber = 0;
+                });
+              }),
+              buildButton(",", () {
+                //TODO add action
+              }),
+              buildButton("=", () {
+                //TODO add action
+              }),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //TODO add action
+            ],
+          )
+        ],
+      ),
+    );
+  }
 
-               ],
-              ),
-                 Column(
-                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                   crossAxisAlignment: CrossAxisAlignment.center,
-                   children: [
-                     Padding(
-                       padding: const EdgeInsets.fromLTRB(8.0,8.0,8.0,8.0),
-                       child: SizedBox(
-                         width: 70,
-                         height: 40,
-                         child: TextButton(onPressed: (){},
-                             style: TextButton.styleFrom(
-                                 side: const BorderSide(width: 1.0)
-                      ),
-                             child: const Text("8")
-                  ),
-                ),
-              ),
-                     Padding(
-                       padding: const EdgeInsets.fromLTRB(8.0,8.0,8.0,8.0),
-                       child: SizedBox(
-                         width: 70,
-                         height: 40,
-                         child: TextButton(onPressed: (){},
-                             style: TextButton.styleFrom(
-                                 side: const BorderSide(width: 1.0)
-                      ),
-                             child: const Text("8")
-                  ),
-                ),
-              ),
-                     Padding(
-                       padding: const EdgeInsets.fromLTRB(8.0,8.0,8.0,8.0),
-                       child: SizedBox(
-                         width: 70,
-                         height: 40,
-                         child: TextButton(onPressed: (){},
-                             style: TextButton.styleFrom(
-                                 side: const BorderSide(width: 1.0)
-                             ),
-                             child: const Text("8")
-                         ),
-                       ),
-                     )
-                   ],
-                 ),],
-           )
-      );
+  Widget buildButton(String buttonText, VoidCallback onPressed) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        width: 70,
+        height: 40,
+        child: TextButton(
+          onPressed: onPressed,
+          style: TextButton.styleFrom(
+            side: const BorderSide(width: 1.0),
+            foregroundColor: Colors.deepOrange
+          ),
+          child: Text(buttonText),
+        ),
+      ),
+    );
   }
 }
-
-
-
